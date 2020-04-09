6127905e02c3a564539bea1100dab2beaa979b3d
Michael Kopsho <michael.kopsho@gmail.com>
2020-04-09 18:31:56 +0000

    Done.

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,3 +1,16 @@
+# def find_min_value(array)
+#   array.sort!
+#   array[0]
+# end
+
 def find_min_value(array)
-  # Add your solution here
+  count = 0
+  minimum = 9999999
+  while count < array.length do
+    if minimum > array[count]
+      minimum = array[count]
+    end
+    count += 1
+  end
+  minimum
 end
